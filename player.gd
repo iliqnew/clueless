2c7241c04f96141370ac3dbf0570a6a419097830
Nikolay Danailov <frostblooded@yahoo.com>
2026-08-24 23:19:22 +0300

    Add moving player

---
+++ player.gd
@@ -0,0 +1,10 @@
+class_name Player
+extends CharacterBody2D
+
+@export var speed: float = 300.0
+
+func _physics_process(_delta: float) -> void:
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * speed
+
+	move_and_slide()
