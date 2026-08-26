d377e318df42c32b08b001fb5b228208f22bbcd5
Nikolay Danailov <frostblooded@yahoo.com>
2026-08-26 22:14:12 +0300

    Add all the basic rooms and connections between them

--- scripts/scene_entry.gd
+++ scripts/scene_entry.gd
@@ -1,12 +1,12 @@
 class_name SceneEntry extends Area2D
 
-@export var next_scene: PackedScene
+@export var next_scene_path: String
 @export var direction: Enums.WorldDirection
 
 func _on_body_entered(body: Node2D) -> void:
-	if body is Player:
-		if next_scene == null:
-			print_debug("Next scene is null, make sure there isn't a circular dependancy that is preventing the attached packed scene from being loaded and remaining null")
+	if next_scene_path.is_empty():
+		return
 
+	if body is Player:
 		Globals.entrance_direction = Enums.get_reverse_world_direction(direction)
-		get_tree().change_scene_to_packed.call_deferred(next_scene)
+		get_tree().change_scene_to_file.call_deferred(next_scene_path)
